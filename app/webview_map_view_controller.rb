478900dcc520cf28d2174ca0c51db4318afc6976
Barrett Clark <barrett@websitesbybarrett.com>
2014-05-18 21:44:22 -0500

    HTML5 map

--- app/webview_map_view_controller.rb
+++ app/webview_map_view_controller.rb
@@ -1,6 +1,11 @@
 class WebviewMapViewController < UIViewController
   def viewDidLoad
     self.title = 'HTML5 Map'
+    self.view = UIWebView.alloc.init
+    path = NSBundle.mainBundle.pathForResource('map', ofType: 'html')
+    url = NSURL.fileURLWithPath(path)
+    request = NSURLRequest.requestWithURL(url)
+    self.view.loadRequest request
     super
   end
 end
